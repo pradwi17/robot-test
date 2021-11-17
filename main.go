c2ac5ee245ef5f083ffa019410e0a7ee3d437e59
dsubarto <dsubarto@gmail.com>
2021-11-17 13:11:02 +0700

    [FEATURE] Robot facing the right direction from command L and R

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 	scanner.Scan()
 	command = strings.Split(scanner.Text(), "")
 	fmt.Println(command)
+	move(command)
 
 }
 
@@ -36,4 +37,46 @@ func initializePosition() (direction string, x int, y int) {
 	scanner.Scan()
 	y, _ = strconv.Atoi(scanner.Text())
 	return direction, x, y
+}
+
+func move(command [] string)  {
+	var turn *string = &direction
+	for i := 0; i < len(command); i++ {
+		switch command[i] {
+		case "L":
+			turnLeft(turn)
+			fmt.Println(direction)
+		case "R":
+			turnRight(turn)
+			fmt.Println(direction)
+		}
+	}	
+}
+
+func turnLeft(turn *string)  {
+	switch *turn {
+	case "N":
+		*turn = "W"
+	case "E":
+		*turn = "N"
+	case "S":
+		*turn = "E"
+	case "W":
+		*turn = "S"
+		
+	}
+}
+
+func turnRight(turn *string)  {
+	switch *turn {
+	case "N":
+		*turn = "E"
+	case "E":
+		*turn = "S"
+	case "S":
+		*turn = "W"
+	case "W":
+		*turn = "N"
+		
+	}
 }
