5b699e73b09d3cc7f3ec38d436cc05cbf15d0603
dsubarto <dsubarto@gmail.com>
2021-11-17 11:47:44 +0700

    [FEATURE] User able to input x Axis for initial position of the robot

--- main.go
+++ main.go
@@ -4,12 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 var scanner = bufio.NewScanner(os.Stdin)
 
+var direction string
+var xAxis int
+var yAxis int
+
 func main() {
-	initializePosition()
+	direction, xAxis, yAxis = 	initializePosition()
+
 }
 
 func initializePosition() (direction string, x int, y int) {
@@ -17,6 +23,9 @@ func initializePosition() (direction string, x int, y int) {
 	fmt.Print("Facing Direction (N / E / S / W): ")
 	scanner.Scan()
 	direction = scanner.Text()
+	fmt.Print("X axis (number): ")
+	scanner.Scan()
+	x, _ = strconv.Atoi(scanner.Text())
 
-	return direction, 0, 0
+	return direction, x, y
 }
